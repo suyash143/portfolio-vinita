293883d8beb080d0f07a4906c27d8bf3073cee52
suyash143 <suyashpathak143@gmail.com>
2023-01-07 12:26:56 +0530

    New settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
